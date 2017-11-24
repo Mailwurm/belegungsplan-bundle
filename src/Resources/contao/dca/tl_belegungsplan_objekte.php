1afacbc7899d72a22f2af3cb2d1609e015cc6d3d
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-24 02:51:20 +0100

    Update tl_belegungsplan_objekte.php

--- src/Resources/contao/dca/tl_belegungsplan_objekte.php
+++ src/Resources/contao/dca/tl_belegungsplan_objekte.php
@@ -298,7 +298,22 @@ class tl_belegungsplan_objekte extends Backend
 				break;
 		}
 	}
-
+	/**
+	 * Return the edit header button
+	 *
+	 * @param array  $row
+	 * @param string $href
+	 * @param string $label
+	 * @param string $title
+	 * @param string $icon
+	 * @param string $attributes
+	 *
+	 * @return string
+	 */
+	public function editHeader($row, $href, $label, $title, $icon, $attributes)
+	{
+		return $this->User->canEditFieldsOf('tl_belegungsplan_objekte') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
+	}
 	/**
 	* Add the type of input field
 	*
