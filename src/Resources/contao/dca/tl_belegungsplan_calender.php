ae8e3a563cae2d8d4f7c744096c421c383a14730
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-24 01:51:59 +0100

    Update tl_belegungsplan_calender.php

--- src/Resources/contao/dca/tl_belegungsplan_calender.php
+++ src/Resources/contao/dca/tl_belegungsplan_calender.php
@@ -59,7 +59,7 @@ $GLOBALS['TL_DCA']['tl_belegungsplan_calender'] = array
 			'edit' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_belegungsplan_calender']['edit'],
-				'href'                => 'table=tl_belegungplan_calender',
+				'href'                => 'table=tl_belegungsplan_calender',
 				'icon'                => 'edit.svg'
 			),
 			'delete' => array
