f24abbf212ada1b4e3ff32c496fd3efc15c3f68f
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-25 03:49:33 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -10,10 +10,10 @@
 /**
 * Add palettes to tl_module
 */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['belegungsplanlist']   = '{title_legend},name,type;{config_legend},belegungsplan_categories;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['belegungsplanlist']   = '{title_legend},name,type;{config_legend},belegungsplan_categories,belegungsplan_month;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 /**
- * Add fields to tl_module
- */
+* Add fields to tl_module
+*/
 $GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_categories'] = array(
 	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['belegungsplan_categories'],
 	'exclude'                 => true,
@@ -23,13 +23,22 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_categories'] = array(
 	'sql'                     => "blob NULL"
 );
 
+$GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_month'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['belegungsplan_month'],
+	'exclude'                 => true,
+	'inputType'               => 'radioTable',
+	'options'                 => $GLOBALS['TL_LANG']['tl_module']['belegungsplan_month']['month'],
+	'eval'                    => array('cols'=>4, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
 $bundles = System::getContainer()->getParameter('kernel.bundles');
 
 /**
- * Provide miscellaneous methods that are used by the data configuration array.
- *
- * @author Jan Karai <https://www.sachsen-it.de>
- */
+* Provide miscellaneous methods that are used by the data configuration array.
+*
+* @author Jan Karai <https://www.sachsen-it.de>
+*/
 class tl_module_belegungsplan extends Backend {
 
 }
