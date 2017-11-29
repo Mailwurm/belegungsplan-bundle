0679b403159207f2858d731c30824101f2ccd145
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-29 21:10:34 +0100

    Create ModuleBelegungsplaene.php

---
+++ src/Resources/contao/modules/ModuleBelegungsplaene.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Contao Open Source CMS
+*
+* Copyright (c) Jan Karai
+*
+* @license LGPL-3.0+
+*/
+namespace Mailwurm;
+use Patchwork\Utf8;
+
+/**
+* Class ModuleBelegungsplaene
+*
+*
+* @author Jan Karai <https://www.sachsen-it.de>
+*/
+class ModuleBelegungsplaene extends \Frontend
+{
+}
