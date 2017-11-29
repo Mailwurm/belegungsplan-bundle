b4e5f0eae750c2c6b3c1ddd491405dc02d67dff3
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-29 21:07:16 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,7 +9,14 @@
 /**
 * Add back end modules
 */
-$GLOBALS['BE_MOD']['content']['belegungsplan']['tables'] = array('tl_belegungsplan_category', 'tl_belegungsplan_objekte', 'tl_belegungsplan_calender');
+array_insert($GLOBALS['BE_MOD']['content'], 1, array
+(
+	'belegungsplan' => array
+	(
+		'tables'      => array('tl_belegungsplan_category', 'tl_belegungsplan_objekte', 'tl_belegungsplan_calender')
+	)
+));
+
 /**
 * Front end modules
 */
