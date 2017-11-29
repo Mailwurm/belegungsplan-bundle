b266b1a7f23d55b794425efa4a5a848758ed3d97
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-11-29 01:24:36 +0100

    Update ModuleBelegungsplan.php

--- src/Resources/contao/modules/ModuleBelegungsplan.php
+++ src/Resources/contao/modules/ModuleBelegungsplan.php
@@ -1,19 +1,1 @@
-<?php
-/**
-* Contao Open Source CMS
-*
-* Copyright (c) Jan Karai
-*
-* @license LGPL-3.0+
-*/
-namespace Mailwurm;
-use Contao\CoreBundle\Exception\PageNotFoundException;
-use Patchwork\Utf8;
-/**
-* Class ModuleBelegungsplan
-*
-* @author Jan Karai <https://www.sachsen-it.de>
-*/
-class ModuleBelegungsplan extends \Frontend {
 
-}
