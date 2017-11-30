ea1e12bd1f235f1d331dd9c418e215e4d73054a5
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-01 00:37:52 +0100

    Update Belegungsplan.php

--- src/Resources/contao/classes/Belegungsplan.php
+++ src/Resources/contao/classes/Belegungsplan.php
@@ -9,10 +9,56 @@
 namespace Mailwurm;
 
 /**
-* Class Belegungsplaene
+* Stellt Methoden bereit, um alle Reservierungsergebnisse eines bestimmten Zeitraums aus der Datenbank abzurufen.
 *
 * @author Jan Karai <https://www.sachsen-it.de>
 */
 abstract class Belegungsplan extends \Module
 {
+	/**
+	* Aktuelle URL
+	* @var string
+	*/
+	protected $strUrl;
+	
+	/**
+	* Aktuelles Jahr Beginn 01.01.???? 00:00:00
+	* @var int
+	*/
+	protected $intActYearBegin;
+	
+	/**
+	* Aktuelles Jahr Ende 31.12.???? 23:59:59
+	* @var int
+	*/
+	protected $intActYearEnd;
+	
+	/**
+	* Aktuelle Reservierungen
+	* @var array
+	*/
+	protected $arrReservierungen = array();
+	
+	/**
+	* URL cache array
+	* @var array
+	*/
+	private static $arrUrlCache = array();
+	
+	/**
+	* Holt alle Reservierungen ab eines bestimmten Zeitraums
+	*
+	* @param array   $arrCalender
+	* @param integer $intStart
+	*
+	* @return array
+	*/
+	protected function getAllReservierungen($arrCalender, $intStart)
+	{
+		$this->arrReservierungen = $arrCalender;
+		
+		return $this->arrReservierungen;
+	}
+	
+	
 }
