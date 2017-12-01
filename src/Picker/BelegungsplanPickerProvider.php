bd8378a929ac0b9516abeb9c9001659cf0ef0248
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-01 09:27:54 +0100

    Update BelegungsplanPickerProvider.php

--- src/Picker/BelegungsplanPickerProvider.php
+++ src/Picker/BelegungsplanPickerProvider.php
@@ -37,12 +37,7 @@ class BelegungsplanPickerProvider extends AbstractPickerProvider implements DcaP
     public function supportsContext($context) {
         return 'link' === $context && $this->getUser()->hasAccess('belegungsplan', 'modules');
     }
-    /**
-    * {@inheritdoc}
-    */
-    public function supportsValue(PickerConfig $config) {
-        return false !== strpos($config->getValue(), '{{belegungsplan_url::');
-    }
+   
     /**
     * {@inheritdoc}
     */
@@ -50,54 +45,5 @@ class BelegungsplanPickerProvider extends AbstractPickerProvider implements DcaP
     {
         return 'tl_belegungsplan_objekte';
     }
-    /**
-    * {@inheritdoc}
-    */
-    public function getDcaAttributes(PickerConfig $config) {
-        $attributes = ['fieldType' => 'radio'];
-        if ($this->supportsValue($config)) {
-            $attributes['value'] = str_replace(['{{belegungsplan_url::', '}}'], '', $config->getValue());
-        }
-        return $attributes;
-    }
-    /**
-    * {@inheritdoc}
-    */
-    public function convertDcaValue(PickerConfig $config, $value) {
-        return '{{belegungsplan_url::'.$value.'}}';
-    }
-    /**
-    * {@inheritdoc}
-    */
-    protected function getRouteParameters(PickerConfig $config = null) {
-        $params = ['do' => 'belegungsplan'];
-        if (null === $config || !$config->getValue() || false === strpos($config->getValue(), '{{belegungsplan_url::')) {
-            return $params;
-        }
-        $value = str_replace(['{{belegungsplan_url::', '}}'], '', $config->getValue());
-        if (null !== ($belegungsplanId = $this->getBelegungsplanCategoryId($value))) {
-            $params['table'] = 'tl_belegungsplan_objekte';
-            $params['id'] = $belegungsplanId;
-        }
-        return $params;
-    }
-    /**
-    * Returns the Belegungsplan category ID.
-    *
-    * @param int $id
-    *
-    * @return int|null
-    */
-    private function getBelegungsplanCategoryId($id)
-    {
-        /** @var BelegungsplanObjekteModel $belegungplanAdapter */
-        $belegungplanAdapter = $this->framework->getAdapter(BelegungsplanObjekteModel::class);
-        if (!($belegungplanModel = $belegungplanAdapter->findById($id)) instanceof BelegungsplanObjekteModel) {
-            return null;
-        }
-        if (!($belegungplanCategory = $belegungplanModel->getRelated('pid')) instanceof BelegungsplanCategoryModel) {
-            return null;
-        }
-        return $belegungplanCategory->id;
-    }
+    
 }
