9c527b5c73b41d61e24e4fda867d4bf586f02ce9
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-03 23:13:01 +0100

    Update ModuleBelegungsplanlist.php

--- src/Resources/contao/modules/ModuleBelegungsplanlist.php
+++ src/Resources/contao/modules/ModuleBelegungsplanlist.php
@@ -18,7 +18,7 @@ use Patchwork\Utf8;
 *
 * @author Jan Karai <https://www.sachsen-it.de>
 */
-class ModuleBelegungsplanlist extends \Belegungsplaene
+class ModuleBelegungsplanlist extends \Belegungsplan
 {
 	/**
 	* Template
