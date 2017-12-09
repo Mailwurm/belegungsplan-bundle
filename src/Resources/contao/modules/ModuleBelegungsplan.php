616f1f6cffaf62a2fecfd7a09ea27e3a434e5703
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-09 07:14:07 +0100

    Update ModuleBelegungsplan.php

--- src/Resources/contao/modules/ModuleBelegungsplan.php
+++ src/Resources/contao/modules/ModuleBelegungsplan.php
@@ -74,10 +74,14 @@ class ModuleBelegungsplan extends \Module
 	protected function compile() 
 	{
 		$arrInfo = array();
-		
 		$arrCategorieObjekte = array();
 		$arrJahre = array();
-
+		$arrObjekteCalender = array();
+		// Monate sortieren
+		$arrBelegungsplanMonth = $this->belegungsplan_month;
+		sort($arrBelegungsplanMonth, SORT_NUMERIC);
+		$this->belegungsplan_month = $arrBelegungsplanMonth;
+		
 		$blnClearInput = false;
 		
 		$intYear = \Input::get('belegyear');
@@ -91,16 +95,96 @@ class ModuleBelegungsplan extends \Module
 			$intYear = date('Y');
 			$blnClearInput = true;
 		} else {
-			if($intYear) {
-				is_numeric($intYear) && strlen($intYear) === 4 ? ($intYear >= date('Y') ? $intYear = intval($intYear) : $arrInfo[] = $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][2]) : $arrInfo[] = $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][1];
+			if(!empty($intYear)) {
+				is_numeric($intYear) && strlen($intYear) === 4 ? ($intYear >= date('Y') ? $intYear = intval($intYear) : $arrInfo[] = '4. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][2]) : $arrInfo[] = '1. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][1];
 			}
-			if($intMonth) {
-				is_numeric($intMonth) && strlen($intMonth) === 6 ? (substr($intMonth, 4) >= date('Y') ? $intMonth = intval($intMonth) : $arrInfo[] = $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][2]) : $arrInfo[] = $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][1];
+			if(!empty($intMonth)) {
+				is_numeric($intMonth) && strlen($intMonth) === 6 ? (intval(substr($intMonth, 0, 4)) >= date('Y') ? $intMonth = intval($intMonth) : $arrInfo[] = '5. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][2]) : $arrInfo[] = '2. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][1];
+				// Pruefen ob Monat angezeigt werden darf
+				!in_array(intval(substr($intMonth, 4, 2)), $this->belegungsplan_month) ?  $arrInfo[] = '6. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][0] : '';
 			}
 		}
 		
 		// wenn $arrInfo hier schon belegt, dann nicht erst weiter machen
 		if(empty($arrInfo)) {
+			$intY = 0;
+			$intM = 0;
+			$intAnzahlTage = 0;
+			// Anfang und Ende des Anzeigezeitraumes je nach GET
+			if(!empty($intYear)) {
+				$intStartAuswahl = mktime(0, 0, 0, 1, 1, intval($intYear));
+				$intEndeAuswahl = mktime(23, 59, 59, 12, 31, intval($intYear));
+			}
+			if(!empty($intMonth)) {
+				$intY = intval(substr($intMonth, 0, 4));
+				$intM = intval(substr($intMonth, 4, 2));
+				$intStartAuswahl = mktime(0, 0, 0, $intM, 1, $intY);
+				$intAnzahlTage = intval(date('t', $intStartAuswahl));
+				$intEndeAuswahl = mktime(23, 59, 59, $intM, $intAnzahlTage, $intY);
+			}
+			
+			// Hole alle Calenderdaten zur Auswahl
+			$objObjekteCalender = $this->Database->prepare("SELECT tbo.id as ObjektID,
+										(CASE
+											WHEN tbc.startDate < ".$intStartAuswahl." THEN DAY(FROM_UNIXTIME(".$intStartAuswahl."))
+											ELSE DAY(FROM_UNIXTIME(tbc.startDate))
+										 END) as StartTag,
+										 (CASE
+											WHEN tbc.startDate < ".$intStartAuswahl." THEN MONTH(FROM_UNIXTIME(".$intStartAuswahl."))
+											ELSE MONTH(FROM_UNIXTIME(tbc.startDate))
+										 END) as StartMonat,
+										 (CASE
+											WHEN tbc.startDate < ".$intStartAuswahl." THEN YEAR(FROM_UNIXTIME(".$intStartAuswahl."))
+											ELSE YEAR(FROM_UNIXTIME(tbc.startDate))
+										 END) as StartJahr,
+										 (CASE
+											WHEN tbc.endDate > ".$intEndeAuswahl." THEN DAY(FROM_UNIXTIME(".$intEndeAuswahl."))
+											ELSE DAY(FROM_UNIXTIME(tbc.endDate))
+										 END) as EndeTag,
+										 (CASE
+											WHEN tbc.endDate > ".$intEndeAuswahl." THEN MONTH(FROM_UNIXTIME(".$intEndeAuswahl."))
+											ELSE MONTH(FROM_UNIXTIME(tbc.endDate))
+										 END) as EndeMonat,
+										 (CASE
+											WHEN tbc.endDate > ".$intEndeAuswahl." THEN YEAR(FROM_UNIXTIME(".$intEndeAuswahl."))
+											ELSE YEAR(FROM_UNIXTIME(tbc.endDate))
+										 END) as EndeJahr
+									FROM 	tl_belegungsplan_calender tbc,
+											tl_belegungsplan_objekte tbo
+									WHERE 	tbc.pid = tbo.id
+									AND 	tbo.published = 1
+									AND 	((startDate < ".$intStartAuswahl." AND endDate > ".$intStartAuswahl.") OR (startDate >= ".$intStartAuswahl." AND endDate <= ".$intEndeAuswahl.") OR (startDate < ".$intEndeAuswahl." AND endDate > ".$intEndeAuswahl."))")
+									->execute();
+			if($objObjekteCalender->numRows > 0) {
+				while($objObjekteCalender->next()) {
+					$arrHelper = array();
+					$arrHelper['ObjektID'] = $objObjekteCalender->ObjektID;
+					$arrHelper['StartTag'] = (int)$objObjekteCalender->StartTag;
+					$arrHelper['StartMonat'] = (int)$objObjekteCalender->StartMonat;
+					$arrHelper['StartJahr'] = (int)$objObjekteCalender->StartJahr;
+					$arrHelper['EndeTag'] = (int)$objObjekteCalender->EndeTag;
+					$arrHelper['EndeMonat'] = (int)$objObjekteCalender->EndeMonat;
+					$arrHelper['EndeJahr'] = (int)$objObjekteCalender->EndeJahr;
+					$intEndeMonat = (int)date('t', mktime(0, 0, 0, $arrHelper['StartMonat'], $arrHelper['StartTag'], $arrHelper['StartJahr']));
+					for($d = $arrHelper['StartTag'], $m = $arrHelper['StartMonat'], $e = $intEndeMonat, $y = $arrHelper['StartJahr']; ; ) {
+						$arrObjekteCalender[$objObjekteCalender->ObjektID][$m][$d] = 1;
+						if($d === $e) {
+							if($arrHelper['StartMonat'] === $arrHelper['EndeMonat']) {
+								break;
+							}
+							$m++;
+							$d = 0;
+							$e = (int)date('t', mktime(0, 0, 0, $m, $d + 1, $y));
+						}
+						if($y === $arrHelper['EndeJahr'] && $m === $arrHelper['EndeMonat'] && $d === $arrHelper['EndeTag']) {
+							break;
+						}
+						$d++;
+					}
+					unset($arrHelper);
+				}
+			}
+			
 			// Hole alle aktiven Objekte inklusive dazugehoeriger Kategorie
 			$objCategoryObjekte = $this->Database->prepare("SELECT 	tbc.id as CategoryID,
 										tbc.title as CategoryTitle,
@@ -118,9 +202,14 @@ class ModuleBelegungsplan extends \Module
 					// Nicht anzuzeigende Kategorien aussortieren
 					if(in_array($objCategoryObjekte->CategoryID, $this->belegungsplan_category)) {
 						$arrHelper = array();
-						$arrHelper['ObjektID'] = $objCategoryObjekte->ObjektID;
+						$arrHelper['ObjektID'] = (int)$objCategoryObjekte->ObjektID;
 						$arrHelper['ObjektName'] = $objCategoryObjekte->ObjektName;
 						$arrHelper['ObjektInfoText'] = $objCategoryObjekte->ObjektInfoText;
+						// Calender anfügen wenn vorhanden
+						if(array_key_exists($arrHelper['ObjektID'], $arrObjekteCalender)) {
+							$arrHelper['Calender'] = $arrObjekteCalender[$arrHelper['ObjektID']];
+							unset($arrObjekteCalender[$arrHelper['ObjektID']]);
+						}
 						if(array_key_exists($objCategoryObjekte->CategoryID, $arrCategorieObjekte)) {
 							$arrCategorieObjekte[$objCategoryObjekte->CategoryID]['Objekte'][$objCategoryObjekte->ObjektSortierung] = $arrHelper;
 							$i++;
@@ -133,9 +222,14 @@ class ModuleBelegungsplan extends \Module
 					}
 				}
 			} else {
-				$arrInfo[] = $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][0];
+				$arrInfo[] = '3. ' . $GLOBALS['TL_LANG']['mailwurm_belegung']['info'][0];
 			}
-		
+			
+			
+			
+			
+			
+			
 			// Hole alle Jahre fuer die bereits Buchungen vorhanden sind ab dem aktuellen Jahr
 			$objJahre = $this->Database->prepare("	SELECT YEAR(FROM_UNIXTIME(startDate)) as Start 
 								FROM tl_belegungsplan_calender 
@@ -145,7 +239,7 @@ class ModuleBelegungsplan extends \Module
 								->execute(date("Y"));
 			if($objJahre->numRows > 0) {
 				while($objJahre->next()) {
-					$arrJahre[] = array('single_year' => $objJahre->Start, 'year_href' => $objJahre->Start == $intYear ? '' : $this->strUrl . '?belegyear=' . $objJahre->Start, 'active' => $objJahre->Start == $intYear ? 1 : 0);
+					$arrJahre[] = array('single_year' => $objJahre->Start, 'year_href' => $this->strUrl . '?belegyear=' . $objJahre->Start, 'active' => $objJahre->Start == $intYear ? 1 : 0);
 				}
 			}
 		}
@@ -163,12 +257,16 @@ class ModuleBelegungsplan extends \Module
 		$this->Template->selectable_year = $arrJahre;
 		// Anzahl anzuzeigender Objekte
 		$this->Template->number_objekte = $i;
-		$this->Template->CategorieObjekteCalender = $arrCategorieObjekte;
+		// Kategorien sortieren wie im Checkboxwizard ausgewaehlt -> Elterntabelle
+		$this->Template->CategorieObjekteCalender = $this->sortNachWizard($arrCategorieObjekte, $this->belegungsplan_category);
 		
 		
 		
-		$this->Template->belegungsplan_category = $this->belegungsplan_category;
-		$this->Template->belegungsplan_month = $this->belegungsplan_month;
+		
+		#$this->Template->belegungsplan_category = $this->belegungsplan_category;
+		$this->Template->Month = $this->dataMonth($arrBelegungsplanMonth, $intStartAuswahl);
+		#$this->Template->Start = $intStartAuswahl;
+		#$this->Template->Ende = $intEndeAuswahl;
 		
 		
 		
@@ -185,5 +283,43 @@ class ModuleBelegungsplan extends \Module
 			\Input::setGet('belegyear', null);
 			\Input::setGet('belegmonth', null);
 		}
+	}
+	
+	/**
+	* Sortiert die Kategorien nach Auswahl im Checkbox-Wizard
+	*
+	* @return array
+	*/
+	protected function sortNachWizard($arrCategorieObjekte, $arrBelegungsplanCategory)
+	{	
+		// Schluessel und Werte tauschen
+		$arrHelper = array_flip($arrBelegungsplanCategory);
+		
+		foreach($arrHelper as $key => $value) {
+			if(array_key_exists($key, $arrCategorieObjekte)) {
+				$arrHelper[$key] = $arrCategorieObjekte[$key];
+				// Objekte in der Kategorie gleich mit nach DB sortieren
+				ksort($arrHelper[$key]['Objekte']);
+			} else {
+				unset($arrHelper[$key]);
+			}
+		}
+		// leere Einträge entfernen
+		return $arrHelper;
+	}
+	
+	/**
+	* Fuegt den Monaten Daten hinzu
+	*
+	* @return array
+	*/
+	protected function dataMonth($arrMonth, $intStartAuswahl)
+	{
+		$arrHelper = array();
+		foreach($arrMonth as $key => $value) {
+			$arrHelper[$value]['Name'] = $GLOBALS['TL_LANG']['mailwurm_belegung']['month'][$value];
+		}
+		return $arrHelper;
+		
 	}
 }
