2d5913dee7caa2a366d2a11f7720970db4676414
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-13 08:42:08 +0100

    Update tl_form_field.php

--- src/Resources/contao/dca/tl_form_field.php
+++ src/Resources/contao/dca/tl_form_field.php
@@ -10,13 +10,13 @@
 /**
 * Add palettes to tl_form_field
 */
-$GLOBALS['TL_DCA']['tl_form_field']['palettes']['belegungsplancategoryselect'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,multiple;{options_legend},options;{expert_legend:hide},class,accesskey,tabindex;{template_legend:hide},customTpl';
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['belegungsplancategoryselect'] = '{type_legend},type,name,label;{options_legend},belegungsplancategorycheck;{expert_legend:hide},class,accesskey,tabindex;{template_legend:hide},customTpl';
 
 /**
 * Add fields to tl_form_field
 */
-$GLOBALS['TL_DCA']['tl_form_field']['fields']['belegungsplancategoryselect'] = array(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['belegungsplancategoryselect'],
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['belegungsplancategorycheck'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['belegungsplancategorycheck'],
 	'exclude'                 => true,
 	'inputType'               => 'checkboxWizard',
 	'foreignKey'              => 'tl_belegungsplan_category.title',
