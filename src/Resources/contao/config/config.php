0e63d2185524fe2348315b0f22a513f65dc9d851
Jan Karai <33639393+Mailwurm@users.noreply.github.com>
2017-12-21 14:30:47 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -44,4 +44,3 @@ if (TL_MODE == 'FE')
 {
 	$GLOBALS['TL_CSS'][] = 'bundles/mailwurmbelegungsplan/belegungsplan.css|static';
 }
-$GLOBALS['TL_MODELS']['tl_belegungsplan_category'] = \Mailwurm\BelegungsplanBundle\Models\BelegungsplanCategoryModel::class;
