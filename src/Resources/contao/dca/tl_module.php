782b204be6c68507afc70c270dce13ece5f6821a
Mailwurm <jankarai73@gmail.com>
2020-12-15 08:24:14 +0100

    Update tl_module.php
    Callback eingefügt

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -36,7 +36,15 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_color_frei'] = array(
 	'exclude'				=> true,
 	'inputType'				=> 'text',
 	'default'				=> '4CAE4C',
+	'load_callback'			=> array
+	(
+		array('tl_module_belegungsplan','setRgbToHex')
+	),
 	'eval'					=> array('maxlength'=>6, 'minlength'=>6, 'mandatory'=>true, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 m12 wizard'),
+	'save_callback'			=> array
+	(
+		array('tl_module_belegungsplan','setHexToRgb')
+	),
 	'sql'					=> "varchar(20) NOT NULL default ''"
 );
 
@@ -55,7 +63,15 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_color_belegt'] = array(
 	'exclude'				=> true,
 	'inputType'				=> 'text',
 	'default'				=> 'D43F3A',
+	'load_callback'			=> array
+	(
+		array('tl_module_belegungsplan','setRgbToHex')
+	),
 	'eval'					=> array('maxlength'=>6, 'minlength'=>6, 'mandatory'=>true, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 m12 wizard'),
+	'save_callback'			=> array
+	(
+		array('tl_module_belegungsplan','setHexToRgb')
+	),
 	'sql'					=> "varchar(20) NOT NULL default ''"
 );
 
@@ -84,10 +100,64 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['belegungsplan_template'] = array(
 $bundles = System::getContainer()->getParameter('kernel.bundles');
 
 /**
-* Provide miscellaneous methods that are used by the data configuration array.
-*
-* @author Jan Karai <https://www.sachsen-it.de>
-*/
+ * Provide miscellaneous methods that are used by the data configuration array.
+ *
+ * @author Jan Karai <https://www.sachsen-it.de>
+ */
 class tl_module_belegungsplan extends Backend {
-
+	/**
+	 * Wandelt Farbcode Hexadezimal nach RGB
+	 *
+	 * @param mixed $varValue
+	 * @param DataContainer $dc
+	 *
+	 * @return mixed
+	 */
+	public function setHexToRgb($varValue, DataContainer $dc)
+	{
+		// Return if there is no active record (override all)
+		if (!$dc->activeRecord)
+		{
+			return;
+		}
+		$hex = str_replace(" ","",$varValue);
+		try
+		{
+			if (strlen($hex) < 6)
+			{	
+				throw new Exception($GLOBALS['TL_LANG']['tl_module']['setHexToRgb']);
+			} else {
+				$r = hexdec(substr($hex,0,2));
+				$g = hexdec(substr($hex,2,2));
+				$b = hexdec(substr($hex,4,2));
+				$rgb = implode(",",array($r, $g, $b));
+				return $rgb;
+			}
+		}
+		catch (\OutOfBoundsException $e)
+		{
+		}
+	}
+	/**
+	 * Wandelt Farbcode RGB nach Hexadezimal
+	 *
+	 * @param mixed $varValue
+	 * @param DataContainer $dc
+	 *
+	 * @return mixed
+	 */
+	public function setRgbToHex($varValue, DataContainer $dc)
+	{
+		// Return if there is no active record (override all)
+		if (!$dc->activeRecord)
+		{
+			return;
+		}
+		$varValue = explode(",",$varValue);
+		$hex = "";
+		$hex .= str_pad(dechex($varValue[0]), 2, "0", STR_PAD_LEFT);
+		$hex .= str_pad(dechex($varValue[1]), 2, "0", STR_PAD_LEFT);
+		$hex .= str_pad(dechex($varValue[2]), 2, "0", STR_PAD_LEFT);
+		return $hex;
+	}
 }
