6c9a625af980df7d20c9c046dcd8d3d434c31f6c
Jan Karai <jankarai73@gmail.com>
2022-07-14 23:26:14 +0200

    Update MailwurmBelegungsplanBundle.php
    Änderung Kommentar

--- src/MailwurmBelegungsplanBundle.php
+++ src/MailwurmBelegungsplanBundle.php
@@ -5,14 +5,14 @@
  * Copyright (c) 2017 Jan Karai
  *
  * @license LGPL-3.0-or-later
+ *
+ * @author Jan Karai <https://www.sachsen-it.de>
  */
 namespace Mailwurm\BelegungsplanBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
- * Configures the Mailwurm Belegungsplan bundle.
- *
- * @author Jan Karai <https://www.sachsen-it.de>
+ * Configures the Mailwurm Belegungsplan bundle
  */
 class MailwurmBelegungsplanBundle extends Bundle {
 
